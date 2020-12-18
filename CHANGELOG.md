d65852cb02185316ae5c3eda3fb0a210f60dd090
Arnaud Schoonjans <arnaud.schoonjans@inmanta.com>
2020-12-18 08:31:16 +0100

    Bump version to 1.2.4

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+# Release 1.2.4 (?)
+
+
 # Release 1.2.3 (2020-09-17)
 
 * Use version 1.3.1 of the Inmanta web-console
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requires = [
 namespace_packages = ["inmanta_ext.ui"]
 
 setup(
-    version="1.2.3",
+    version="1.2.4",
     python_requires=">=3.6",  # also update classifiers
     # Meta data
     name="inmanta-ui",
