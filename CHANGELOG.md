fd3f18ba0643be462da91ed4fea3b8502f4ba795
Arnaud Schoonjans <arnaud.schoonjans@inmanta.com>
2020-12-18 08:28:34 +0100

    Bump version to 1.3.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+# Release 1.3.1 (?)
+
+
 # Release 1.3.0 (2020-12-17)
 
 - Dependency updates
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requires = [
 namespace_packages = ["inmanta_ext.ui"]
 
 setup(
-    version="1.3.0",
+    version="1.3.1",
     python_requires=">=3.6",  # also update classifiers
     # Meta data
     name="inmanta-ui",
