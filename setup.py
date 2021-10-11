8f7b3e3dd15b9139c9c7231fd134ff662d281e4f
inmantaci <ci@inmanta.com>
2021-10-11 13:08:25 +0200

    Version bump based on change entries

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ requires = [
 namespace_packages = ["inmanta_ext.ui"]
 
 setup(
-    version="1.3.3",
+    version="1.4.0",
     python_requires=">=3.6",  # also update classifiers
     # Meta data
     name="inmanta-ui",
