70bca27c82e3b24db7bb497278a20360b91e72b6
inmantaci <ci@inmanta.com>
2022-08-02 15:51:22 +0200

    Increment dev version after RC release

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ requires = [
 namespace_packages = ["inmanta_ext.ui"]
 
 setup(
-    version="1.4.2",
+    version="1.4.3",
     python_requires=">=3.6",  # also update classifiers
     # Meta data
     name="inmanta-ui",
