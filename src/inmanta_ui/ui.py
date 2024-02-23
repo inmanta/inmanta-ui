dcbd8d30d45f7d304054da5fe1b643398bbbd69a
Bart Vanbrabant <bart.vanbrabant@inmanta.com>
2024-02-23 08:44:03 +0100

    Add auth method to config.js (PR #536)
    # Description
    
    Return the auth method in the config. I used a container with the two methods to verify that we create the correct config.
    
    # Self Check:
    
    Strike through any lines that are not applicable (`~~line~~`) then check the box
    
    - [ ] Attached issue to pull request
    - [ ] Changelog entry
    - [ ] Type annotations are present
    - [ ] Code is clear and sufficiently documented
    - [ ] No (preventable) type errors
    - [ ] Sufficient test cases (reproduces the bug/tests the requested feature)
    - [ ] Correct, in line with design
    - [ ] End user documentation is included or an issue is created for end-user documentation (add ref to issue here: )

--- src/inmanta_ui/ui.py
+++ src/inmanta_ui/ui.py
@@ -72,12 +72,24 @@ class UISlice(ServerSlice):
 
         config_js_content = ""
         if opt.server_enable_auth.get():
-            config_js_content = f"""
-        window.auth = {{
-            'realm': '{oidc_realm.get()}',
-            'url': '{oidc_auth_url.get()}',
-            'clientId': '{oidc_client_id.get()}'
-        }};\n"""  # Use the same client-id as the dashboard
+            if opt.server_auth_method.get() == "oidc":
+                config_js_content = f"""
+                    window.auth = {{
+                        'method': 'oidc',
+                        'realm': '{oidc_realm.get()}',
+                        'url': '{oidc_auth_url.get()}',
+                        'clientId': '{oidc_client_id.get()}'
+                    }};\n"""  # Use the same client-id as the dashboard
+            elif opt.server_auth_method.get() == "database":
+                config_js_content = """
+                    window.auth = {{
+                        'method': 'database',
+                    }};\n"""  # Use the same client-id as the dashboard
+            else:
+                raise Exception(
+                    f"Invalid value for config option server.auth_method: {opt.server_auth_method.get()}. "
+                    "Expected either 'oidc' or 'database'."
+                )
 
         config_js_content += f"\nexport const features = {json.dumps(web_console_features.get())};\n"
 
